d52a170dc91eab0f2af5d5c80256bcdcaf5474ff
Daniel Bluhm <dbluhm@pm.me>
2025-03-27 23:35:01 -0500

    test: credential status
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- interop/tests/test_didkit.py
+++ interop/tests/test_didkit.py
@@ -94,7 +94,66 @@ async def credential(controller: Controller, indy_did: str):
         },
     }
     result = await controller.post("/vc/credentials/issue", json=credential)
-    print(json.dumps(result["verifiableCredential"], indent=2))
+    return result["verifiableCredential"]
+
+
+@pytest_asyncio.fixture
+async def credential_with_status(controller: Controller, indy_did: str):
+    """Produce a credential for testing."""
+    vm = indy_did + "#assert"
+    credential = {
+        "credential": {
+            "@context": [
+                "https://www.w3.org/2018/credentials/v1",
+                "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.3.json",
+                "https://www.w3.org/ns/credentials/status/v1",
+                "https://w3id.org/security/suites/ed25519-2020/v1",
+            ],
+            "name": "JFF x vc-edu PlugFest 3 Interoperability",
+            "id": "urn:uuid:8f0df0cc-b5ab-48fa-8bc8-1bac515008cb",
+            "type": ["VerifiableCredential", "OpenBadgeCredential"],
+            "issuer": {
+                "type": ["Profile"],
+                "id": indy_did,
+                "name": "JFF x vc-edu PlugFest 3 Interoperability",
+            },
+            "issuanceDate": "2024-08-21T18:20:23Z",
+            "credentialSubject": {
+                "id": "did:key:z6MktsCtkJCST2NUZfa3SQSm4DL89YmFEdJmt37Vvkw8aH19",
+                "type": ["AchievementSubject"],
+                "achievement": {
+                    "name": "JFF x vc-edu PlugFest 3 Interoperability",
+                    "description": "This wallet supports the use of W3C Verifiable Credentials and has demonstrated interoperability during the presentation request workflow during JFF x VC-EDU PlugFest 3.",
+                    "criteria": {
+                        "type": "Criteria",
+                        "narrative": "Wallet solution providers earned this badge by demonstrating interoperability during the presentation request workflow. This included successfully receiving a presentation request, allowing the holder to select at least two types of verifiable credentials to create a verifiable presentation, returning the presentation to the requester, and passing verification of the presentation and the included credentials.",
+                    },
+                    "image": {
+                        "id": "https://w3c-ccg.github.io/vc-ed/plugfest-3-2023/images/JFF-VC-EDU-PLUGFEST3-badge-image.png",
+                        "type": "Image",
+                    },
+                    "type": ["Achievement"],
+                    "id": "urn:uuid:53b3803c-8774-4697-a614-455588181966",
+                },
+            },
+            "credentialStatus": {
+                "id": "https://mepeltierindicio.share.zrok.io/json-ld-revocation/tenants/d2045eb4-c3c2-411a-b1c1-2406d3f0cb64/w3c/status/0#114976",
+                "type": "BitstringStatusListEntry",
+                "statusPurpose": "revocation",
+                "statusListIndex": 114976,
+                "statusListCredential": "https://mepeltierindicio.share.zrok.io/json-ld-revocation/tenants/d2045eb4-c3c2-411a-b1c1-2406d3f0cb64/w3c/status/0"
+            },
+        },
+        "options": {
+            "challenge": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "created": "2021-12-31T23:59:59Z",
+            "domain": "example.com",
+            "proofPurpose": "assertionMethod",
+            "proofType": "Ed25519Signature2020",
+            "verificationMethod": vm,
+        },
+    }
+    result = await controller.post("/vc/credentials/issue", json=credential)
     return result["verifiableCredential"]
 
 
@@ -107,16 +166,24 @@ async def test_didkit_verify(credential: dict, resolver: Callable, contexts: dic
 
 
 @pytest.mark.asyncio
-async def test_verify_tampered(credential: dict, resolver: Callable, contexts: dict):
-    """Tamper with credential and see what happens"""
-    credential["credentialSubject"]["achievement"]["name"] = "Doctor of Something"
-    print(json.dumps(credential, indent=2))
-    result = wrapper.verify_credential(credential, {}, resolver, contexts)
+async def test_didkit_verify_with_status(credential_with_status: dict, resolver: Callable, contexts: dict):
+    """Test didkit verification of a credential."""
+    result = wrapper.verify_credential(credential_with_status, {}, resolver, contexts)
     assert not result["warnings"]
     assert not result["errors"]
 
 
 @pytest.mark.asyncio
+async def test_verify_tampered(credential: dict, resolver: Callable, contexts: dict):
+    """Tamper with credential and see what happens"""
+    credential["credentialSubject"]["achievement"]["name"] = "Doctor of Something"
+    result = wrapper.verify_credential(credential, {}, resolver, contexts)
+    assert not result["warnings"]
+    assert result["errors"]
+
+
+@pytest.mark.asyncio
+@pytest.mark.xfail(reason="This credential fails to verify for some reason")
 async def test_verify_known_bad(contexts: dict):
     credential = {
         "@context": [
